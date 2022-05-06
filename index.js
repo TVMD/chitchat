8a75f8ab53ac62c7a71b791ce799c9ebadb854ec
Minh <minh@codelynx.io>
2022-05-06 08:59:50 +0700

    hardcode username

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+io.on("connection", (socket) => {
+  socket.on("chat message", (username, message) => {
+    if (!message) {
+      io.emit("chat message", "minh-server", "nope");
+      return;
+    }
+    io.emit("chat message", username, message);
   });
 });
 
